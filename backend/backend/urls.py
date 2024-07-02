a1a4f4f8c6f811e46539810a3c2854012bf40e06
Levan Tene <levan.tene10@gmail.com>
2024-07-02 22:24:15 +0400

    changes made for url reshuffle

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('base.urls')),
+    path('latest_transactions/', include('base.urls')),
     path('', include('blog.urls')),
     path("select2/", include("django_select2.urls")),
     path('admin/', admin.site.urls)
--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/',
+    path('',
          views.blog,
          name='blog'),
-    path('blog/should_you_invest_in_real_estate_in_dubai/',
+    path('should_you_invest_in_real_estate_in_dubai/',
          views.post_one,
          name='post_one'),
-    path('blog/most_expensive_villas_in_dubai/',
+    path('most_expensive_villas_in_dubai/',
          views.post_two,
          name='post_two'),
 ]
